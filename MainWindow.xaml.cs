ab60e6d9d3e3b3d3fa40225a60bdd77208feaabb
AipNooBest <47401054+AipNooBest@users.noreply.github.com>
2022-02-11 20:26:07 +0300

    Насоздавал говна, теперь удалил его

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -17,6 +17,7 @@ namespace Takira
     {
         public Dictionary<string, QuestPage> Story;
         public string CurrentPageHeader;
+        public Stack<string> PageHistory = new Stack<string>();
         public void SetButtons(QuestPage page)
         {
             this.Answers.Children.Clear();
@@ -36,8 +37,7 @@ namespace Takira
             buttonText.Inlines.AddRange(QuestParseHandler.ApplyFormatting(rename));
             button.Content = buttonText;
             // Связываем кнопку со страницей, на которую она ссылается
-            button.Tag = new object[] {this, header, rename};
-            button.Click += new RoutedEventHandler(ButtonClickHandler.SwitchPage);
+            button.Click += new RoutedEventHandler(SwitchPage_OnClick);
             this.Answers.Children.Add(button);
         }
 
@@ -66,14 +66,35 @@ namespace Takira
             
         }
 
+        /// <summary>
+        /// Меняет страницу квеста на другую по названию страницы.
+        /// </summary>
+        /// <param name="window">Объект квестового окна</param>
+        /// <param name="header">Название страницы</param>
+        ///
+        public void SetPage(string header)
+        {
+            QuestPage page = Story[header];
+            SetButtons(page);
+            SetText(page.text);
+            CurrentPageHeader = header;
+        }
+
         public MainWindow()
         {
             InitializeComponent();
             this.ResizeMode = ResizeMode.NoResize;
             Story = QuestParseHandler.LoadQuestFromFile("Realmnauts.Act1.Printer.tw");
-            PageSwitchingHandler.SetPage(this, Story.ElementAt(0).Key);
+            SetPage(Story.ElementAt(0).Key);
             Save.Source = ImageHelper.ConvertImage(Icons.Save, 35, 35);
             Load.Source = ImageHelper.ConvertImage(Icons.Load, 35, 30);
+        }
+        
+        private void SwitchPage_OnClick(object sender, RoutedEventArgs e)
+        {
+            Button button = sender as Button;
+            object[] tags = button.Tag as object[];
+            SetPage((string)tags[1]);
         }
 
         private void Save_OnClick(object sender, RoutedEventArgs e)
